83fa55ea332da2a8bab259084c23e493027d3f90
Linus Unnebäck <linus@folkdatorn.se>
2020-05-29 14:38:39 +0100

    Replace with Expo implementation

--- App.js
+++ App.js
@@ -1,28 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Alert,
-} from 'react-native';
-
-import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
-
+import { Alert, Button, StyleSheet, View } from 'react-native';
 import {getItemAsync, setItemAsync} from 'expo-secure-store';
 
-const App: () => React$Node = () => {
+export default function App() {
   const handleWrite = async () => {
     const value = Math.random().toFixed(6).slice(-6);
     await setItemAsync('session-token', value);
@@ -35,65 +15,18 @@ const App: () => React$Node = () => {
   };
 
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <Button title="Write value" onPress={handleWrite} />
-            <Button title="Read value" onPress={handleRead} />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.container}>
+      <Button title="Write value" onPress={handleWrite} />
+      <Button title="Read value" onPress={handleRead} />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
